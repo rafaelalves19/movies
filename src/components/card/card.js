9b113a2265f72c4f78e9d08da7a56130c9be55bb
Rafael Silva <rafa3lalv3s19@gmail.com>
2022-09-15 19:57:17 +0100

    movie slider in mobile

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,41 +7,41 @@ export default function Card() {
   return (
     <div className="card__moviesSlider">
       <div className="card__background">
-        <img src="/images/exemplo1.JPG" alt="foto" />
+        <img
+          className="card__backgroundImage"
+          src="/images/exemplo1.JPG"
+          alt="foto"
+        />
       </div>
       <div className="card__infoWrapper">
-        <Grid>
-          <div className="card__moviePicture">
+        <div className="card__moviePicture">
+          <Grid>
             <img
               className="card__movieImage"
               src="/images/exemplo4.JPG"
               alt="img"
             />
-          </div>
-          <Container>
             <div className="card__info">
-              <h1>O burro sou eu!</h1>
-              <ul className="card__genres">
-                <li>Terror</li>
-                <li>Científico</li>
-                <li>Romance</li>
-                <li>Comédia</li>
-              </ul>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                pulvinar mi sed quam ullamcorper rutrum. Vivamus tempus erat ut
-                ipsum dictum, in aliquam turpis cursus. Duis justo lectus,
-                elementum at sollicitudin eget, maximus sit amet enim. Donec sed
-                libero justo. In enim sapien, blandit eu vulputate sed, viverra
-                vitae orci. Nulla tellus lacus, posuere nec neque vel, interdum
-                consequat sem.Duis justo lectus, elementum at sollicitudin eget,
-                maximus sit amet enim. Donec sed libero justo. In enim sapien,
-                blandit eu vulputate sed, viverra vitae orci. Nulla tellus
-                lacus, posuere nec neque vel, interdum consequat sem.
-              </p>
+              <Container>
+                <h1>O burro sou eu!</h1>
+                <ul className="card__genres">
+                  <li>Terror</li>
+                  <li>Científico</li>
+                  <li>Romance</li>
+                  <li>Comédia</li>
+                </ul>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  pulvinar mi sed quam ullamcorper rutrum. Vivamus tempus erat
+                  ut ipsum dictum, in aliquam turpis cursus. Duis justo lectus,
+                  elementum at sollicitudin eget, maximus sit amet enim. Donec
+                  sed libero justo. In enim sapien, blandit eu vulputate sed,
+                  viverra vitae orci.
+                </p>
+              </Container>
             </div>
-          </Container>
-        </Grid>
+          </Grid>
+        </div>
       </div>
     </div>
   );
