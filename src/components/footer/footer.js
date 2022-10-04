73d3d1c55502ea361f2249f2d2d5a679c3ab98ca
Rafael Silva <rafa3lalv3s19@gmail.com>
2022-10-04 20:09:12 +0100

    footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,37 @@
-import Container from '../container/container'
+import Popcorn from "../icons/popcorn";
+import Grid from "../grid/grid";
+import Racket from "../icons/racket";
+import { Link } from "react-router-dom";
+import Github from "../icons/github";
+import Linkedin from "../icons/linkedin";
 
 export default function Footer() {
-	return (
-		<footer>
-			<Container>
-				<ul>
-					<li>link</li>
-					<li>link</li>
-					<li>link</li>
-				</ul>
-			</Container>
-		</footer>
-	)
+  return (
+    <footer>
+      <Grid>
+        <div className="footer__Icon1">
+          <Popcorn />
+        </div>
+        <div className="footer__Icon2">
+          <Racket />
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/series">Series</Link>
+          </li>
+        </ul>
+        <Github />
+        <Linkedin />
+      </Grid>
+    </footer>
+  );
 }
--- src/components/slide/slide.js
+++ src/components/slide/slide.js
@@ -1,61 +1,62 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination } from 'swiper'
-import 'swiper/css'
-import 'swiper/css/pagination'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const ItemCard = ({ data }) => {
-	return (
-		<div className='slide__card'>
-			<div className='slide__cardHover'>
-				<h3>{data.title}</h3>
-				<p>Terror</p>
-				<div className='slide__cardHidden'>
-					<p>2022</p>
-					<p>1h 50m</p>
-				</div>
-			</div>
-			<img src={data.cover} />
-		</div>
-	)
-}
+  return (
+    <div className="slide__card">
+      <div className="slide__cardHover">
+        <h3>{data.title}</h3>
+        <p>Terror</p>
+        <div className="slide__cardHidden">
+          <p>2022</p>
+          <p>1h 50m</p>
+        </div>
+      </div>
+      <img src={data.cover} alt="movie" />
+    </div>
+  );
+};
 
 function Slide({ data }) {
-	return (
-		<>
-			<Swiper
-				slidesPerView={1}
-				spaceBetween={10}
-				pagination={{
-					clickable: true,
-				}}
-				breakpoints={{
-					400: {
-						slidesPerView: 2,
-						spaceBetween: 20,
-					},
-					640: {
-						slidesPerView: 2,
-						spaceBetween: 20,
-					},
-					768: {
-						slidesPerView: 4,
-						spaceBetween: 40,
-					},
-					1024: {
-						slidesPerView: 5,
-						spaceBetween: 50,
-					},
-				}}
-				modules={[Pagination]}
-				className='mySwiper'>
-				{data.map((item, index) => (
-					<SwiperSlide>
-						<ItemCard key={index} data={item} />
-					</SwiperSlide>
-				))}
-			</Swiper>
-		</>
-	)
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          100: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {data.map((item, index) => (
+          <SwiperSlide>
+            <ItemCard key={index} data={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
 
-export default Slide
+export default Slide;
