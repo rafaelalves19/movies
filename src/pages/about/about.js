888665edd375a031582e3a49fa028bc85bace9cf
Rafael Silva <rafa3lalv3s19@gmail.com>
2022-11-04 18:39:00 +0000

    styling about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,30 +1,42 @@
+import Container from "../../components/container/container";
+import Grid from "../../components/grid/grid";
+
 export default function About() {
   return (
     <div className="about">
-      <section className="about__project">
-        <h1>About the project</h1>
-        <p>
-          This work aimed to improve my programming skills, using React JS and
-          Sass. <br />
-          You can see the code in my {""}
-          <a target="blank" href="https://github.com/rafaelalves19/movies">
-            repository
-          </a>
-          .
-        </p>
-      </section>
-      <section className="about__me">
-        <h1>About me</h1>
-        <p>
-          My name is Rafael and I'm a front-end developer. <br />
-          I'm learning and improving my developer skills. <br /> You can see my
-          profile{" "}
-          <a target="blank" href="https://www.linkedin.com/in/rafaelalves19/">
-            here
-          </a>
-          .
-        </p>
-      </section>
+      <Container>
+        <Grid>
+          <section className="about__project">
+            <h1>About the project</h1>
+            <p>
+              This work aimed to improve my programming skills, using React JS
+              and Sass. <br></br>Full responsive website.
+              <br />
+              <br />
+              You can see the code in my {""}
+              <a target="blank" href="https://github.com/rafaelalves19/movies">
+                repository
+              </a>
+              .
+            </p>
+          </section>
+          <section className="about__me">
+            <h1>About me</h1>
+            <p>
+              My name is Rafael and I'm a front-end developer. <br />
+              I'm learning and improving my developer skills. <br />
+              <br /> You can see my profile{" "}
+              <a
+                target="blank"
+                href="https://www.linkedin.com/in/rafaelalves19/"
+              >
+                here
+              </a>
+              .
+            </p>
+          </section>
+        </Grid>
+      </Container>
     </div>
   );
 }
